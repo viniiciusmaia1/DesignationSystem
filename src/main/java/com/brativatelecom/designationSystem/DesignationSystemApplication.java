12053d67a413033858ba0ea9cc44562b73d4511e
Viniatv <vinicius99santos.vs@gmail.com>
2024-09-16 12:23:47 -0300

    Firt commit, create project

---
+++ src/main/java/com/brativatelecom/designationSystem/DesignationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.brativatelecom.designationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brativatelecom/designationSystem/DesignationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brativatelecom.designationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
